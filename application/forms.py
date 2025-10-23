a798c5f8d68816ea2c577eee1488b5204bdceeea
nerdguy-lab <joshuamutiso814@gmail.com>
2025-10-23 23:31:02 +0300

    Replace stingfield with passwordfield

--- application/forms.py
+++ application/forms.py
@@ -4,14 +4,14 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Login")
 
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
-    password_confirm = StringField("Confirm Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password_confirm = PasswordField("Confirm Password", validators=[DataRequired()])
     first_name = StringField("First Name", validators=[DataRequired()])
     last_name = StringField("Last Name", validators=[DataRequired()])
     submit = SubmitField("Register Now")
