748f18ffe142200c1e41f1aef35611d4bff45879
nerdguy-lab <joshuamutiso814@gmail.com>
2025-10-23 23:29:40 +0300

    Update mongodb settings

--- config.py
+++ config.py
@@ -3,4 +3,9 @@ import os
 class Config(object):
     SECRET_KEY =os.environ.get('SECRET_KEY') or "secret_string"
 
-    MONGODB_SETTINGS = { 'db' : 'UTA_Enrollment'}
+    MONGODB_SETTINGS = { 
+        "db" : "UTA_Enrollment", #database name
+        "host": "localhost", # Or your MongoDB host
+        "port": 27017,   # Or your MongoDB port
+        "alias": "default",
+    }
