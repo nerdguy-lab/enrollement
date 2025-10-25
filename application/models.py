98f3b56ce45378cddb28e703f335d87c796571b7
nerdguy-lab <joshuamutiso814@gmail.com>
2025-10-25 13:23:50 +0300

    Processing form data and updating the database

--- application/models.py
+++ application/models.py
@@ -1,13 +1,19 @@
 import flask
 import application as db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Document):
     user_id     =   db.IntField( unique=True )
     first_name  =   db.StringField( max_length=50 )
     last_name   =   db.StringField( max_length=50 )
-    email       =   db.EmailField( max_length=30 )
+    email       =   db.EmailField( max_length=30, unique=True )
     password    =   db.StringField( max_length=30 )
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def get_password(self, password):
+        return check_password_hash(self.password, password)
 
 class Course(db.Document):
     course_id = db.StringField( max_length=10, unique=True )
